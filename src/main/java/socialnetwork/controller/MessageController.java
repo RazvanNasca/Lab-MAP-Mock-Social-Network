a5c697f928a95e969d66ce96337f878bed80473f
unknown <perteaale@gmail.com>
2020-12-09 18:19:10 +0200

    Added message chat

--- src/main/java/socialnetwork/controller/MessageController.java
+++ src/main/java/socialnetwork/controller/MessageController.java
@@ -2,21 +2,23 @@ package socialnetwork.controller;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import socialnetwork.config.ApplicationContext;
 import socialnetwork.domain.*;
 import socialnetwork.domain.validators.AccountValidator;
 import socialnetwork.domain.validators.FriendshipValidator;
+import socialnetwork.domain.validators.MessageValidator;
 import socialnetwork.domain.validators.UserValidator;
 import socialnetwork.repository.Repository;
-import socialnetwork.repository.database.AccountDatabaseRepository;
-import socialnetwork.repository.database.FriendRequestDatabaseRepository;
-import socialnetwork.repository.database.FriendshipDatabaseRepository;
-import socialnetwork.repository.database.UserDatabaseRepository;
+import socialnetwork.repository.database.*;
 import socialnetwork.service.FriendshipService;
+import socialnetwork.service.MessageService;
 import socialnetwork.service.UserService;
 
 import java.time.LocalDateTime;
@@ -25,13 +27,19 @@ import java.util.List;
 import java.util.Map;
 
 public class MessageController {
-    public TableView friendsTable;
-    public TableColumn friendColumnFirstName;
-    public TableColumn friendColumnLastName;
+    public TableView<User> friendsTable;
+    public TableColumn<User, String> friendColumnFirstName;
+    public TableColumn<User, String> friendColumnLastName;
+    public ListView<String> messageList;
+    public TextField messageField;
 
     private FriendshipService friendshipService;
+    private MessageService messageService;
     private UserService userService;
     ObservableList<User> modelFriends = FXCollections.observableArrayList();
+    ObservableList<String> messages = FXCollections.observableArrayList();
+
+    User friend;
 
     public void initFriends()
     {
@@ -40,6 +48,40 @@ public class MessageController {
         for (Map.Entry<User,LocalDateTime> entry : allFriends.entrySet())
             friendsList.add(entry.getKey());
         modelFriends.setAll(friendsList);
+    }
+
+    public void initMessages(User friend)
+    {
+        List<Message> conversation = this.messageService.getConversation(HomeController.activeUser.getId(), friend.getId());
+        List<String> list = new ArrayList<>();
+        for (Message x : conversation)
+        {
+            if(x.getFrom().equals(HomeController.activeUser.getId()))
+                list.add("Me: " + x.getMessage());
+            else
+                list.add(this.userService.findUser(x.getFrom()).getFirstName() + ": " + x.getMessage());
+        }
+        messages.setAll(list);
+
+        /*
+        this.messageList.getItems().clear();
+        List<Message> conversation = this.messageService.getConversation(HomeController.activeUser.getId(), friend.getId());
+        this.modelMessages.setAll(conversation);
+
+        messageList.setCellFactory(listView -> new ListCell<Message>()
+        {
+            @Override
+            protected void updateItem(Message item, boolean empty)
+            {
+                super.updateItem(item, empty);
+                if (item == null || empty)
+                    setText(null);
+                else
+                    setText(userService.findUser(item.getFrom()).getFirstName() + ": " + item.getMessage());
+            }
+        });
+
+         */
     }
 
     @FXML
@@ -53,16 +95,46 @@ public class MessageController {
         Repository<Tuple<Long, Long>, Friendship> friendshipDatabaseRepository = new FriendshipDatabaseRepository(url, username, password);
         Repository<String, Account> accountDatabaseRepository = new AccountDatabaseRepository(url, username, password);
         Repository<Tuple<Long, Long>, FriendRequest> friendRequestDatabaseRepository = new FriendRequestDatabaseRepository(url, username, password);
+        Repository<Long, Message> messageDatabaseRepository = new MessageDatabaseRepository(url, username, password);
 
         this.userService = new UserService(userDatabaseRepository, friendshipDatabaseRepository, accountDatabaseRepository, new UserValidator(), new AccountValidator());
-         this.friendshipService = new FriendshipService(userDatabaseRepository, friendRequestDatabaseRepository,friendshipDatabaseRepository, new FriendshipValidator());
+        this.friendshipService = new FriendshipService(userDatabaseRepository, friendRequestDatabaseRepository,friendshipDatabaseRepository, new FriendshipValidator());
+        this.messageService = new MessageService(messageDatabaseRepository, friendshipDatabaseRepository, new MessageValidator());
 
         friendColumnFirstName.setCellValueFactory(new PropertyValueFactory<User, String>("firstName"));
         friendColumnLastName.setCellValueFactory(new PropertyValueFactory<User, String>("lastName"));
 
-
         friendsTable.setItems(modelFriends);
         initFriends();
-    }
 
+        this.friendsTable.setOnMouseClicked(new EventHandler<MouseEvent>()
+        {
+            @Override
+            public void handle(MouseEvent event)
+            {
+                friend = (User)friendsTable.getSelectionModel().getSelectedItem();
+                if(friend != null)
+                {
+                    initMessages(friend);
+                    messageList.setItems(messages);
+                }
+            }
+        });
+
+        this.messageField.setOnKeyPressed(new EventHandler<KeyEvent>()
+        {
+            @Override
+            public void handle(KeyEvent event)
+            {
+                if(event.getCode().equals(KeyCode.ENTER))
+                {
+                    String text = messageField.getText();
+                    ArrayList<Long> to = new ArrayList<>();
+                    to.add(friend.getId());
+                    Message message = new Message(HomeController.activeUser.getId(), to, text, LocalDateTime.now());
+                    messageService.sendMessage(message);
+                }
+            }
+        });
+    }
 }
--- src/main/java/socialnetwork/ui/MainFX.java
+++ src/main/java/socialnetwork/ui/MainFX.java
@@ -25,7 +25,6 @@ public class MainFX extends Application
         stage.getIcons().add(new Image("/images/login.png"));
         stage.show();
     }
-    ////Comentariu de verificare
 
     public static void main(String[] args)
     {
